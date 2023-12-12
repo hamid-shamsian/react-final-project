879c395072410e798258cf04b2315fc60a1ec265
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-12 14:17:27 +0330

    feat: added a simple form validation to LoginPage form +toast warning.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,11 +10,12 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
+import { toast } from "react-toastify";
 import CustomLink from "../components/common/CustomLink";
 import RTLTextField from "../components/common/RTLTextField";
 import authService from "../services/authService";
 import { userActions } from "../redux/features/userSlice";
-import LoginImage from "../assets/images/login.jpg";
+import loginImage from "../assets/images/login.webp";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -24,13 +25,17 @@ const LoginPage = () => {
     e.preventDefault();
     const { username, password } = e.currentTarget;
 
-    try {
-      const user = await authService.login(username.value, password.value);
+    if (username.value && password.value) {
+      try {
+        const user = await authService.login(username.value, password.value);
 
-      dispatch(userActions.setUser(user));
-      if (user.role === "ADMIN") navigate("/admin");
-      else navigate("/");
-    } catch (error) {}
+        dispatch(userActions.setUser(user));
+        if (user.role === "ADMIN") navigate("/admin");
+        else navigate("/");
+      } catch (error) {}
+    } else {
+      toast.warn("لطفا فیلدهای نام‌کاربری و کلمه‌عبور را پر کنید!");
+    }
   };
 
   return (
@@ -41,7 +46,7 @@ const LoginPage = () => {
         sm={4}
         md={7}
         sx={{
-          backgroundImage: `url(${LoginImage})`,
+          backgroundImage: `url(${loginImage})`,
           backgroundRepeat: "no-repeat",
           backgroundColor: t => (t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900]),
           backgroundSize: "cover",
