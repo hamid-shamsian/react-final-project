8a2da29f3bd048468ad895bb4510696fbd1df065
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-12 23:23:59 +0330

    feat: React Query installed and configured.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { RouterProvider } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 import router from "./routes/routes";
 import store from "./redux/store";
 import "react-toastify/dist/ReactToastify.css";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
     <ReduxProvider store={store}>
-      <RouterProvider router={router} />
-      <ToastContainer rtl />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer rtl />
+      </QueryClientProvider>
     </ReduxProvider>
   );
 };
