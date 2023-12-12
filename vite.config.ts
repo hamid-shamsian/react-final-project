5e7a349e539b06637c385b52937daaa05c39702a
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-12 13:07:13 +0330

    feat: Configured Proxy (vite) for Dev Env. in order to Cookies can be set while CORS policy complains.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import config from "./config.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxying API requests
+      "/proxied": {
+        target: config.BACKEND_BASE_URL, // Backend server URL
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/proxied/, "")
+      }
+    }
+  }
+});
