704a70122c3aad30cdaf85ef88aef4171a7b8791
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-12 23:55:05 +0330

    feat: AdminProductsPage reimplemented using the useProducts hook and Pagination component.

--- src/pages/admin/AdminProductsPage.tsx
+++ src/pages/admin/AdminProductsPage.tsx
@@ -1,41 +1,19 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Pagination from "@mui/material/Pagination";
-import PaginationItem from "@mui/material/PaginationItem";
 import Button from "@mui/material/Button";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import StripedTable from "../../components/widget/StripedTable";
-import productService from "../../services/productService";
-import subCatService from "../../services/subCatService";
 import config from "../../../config.json";
-
-interface productType {
-  category: string;
-  createdAt: string;
-  description: string;
-  images: string[];
-  name: string;
-  price: number;
-  quantity: number;
-  rating: {
-    rate: number;
-    count: number;
-  };
-  slugname: string;
-  subcategory: string;
-  thumbnail: string;
-  updatedAt: string;
-  _id: string;
-}
+import Pagination from "../../components/common/Pagination";
+import LoadingSpinner from "../../components/common/LoadingSpinner";
+import useProducts, { Product } from "../../hooks/useProducts";
 
 const tableColumns = [
   {
     label: "تصویر",
     key: "thumbnail",
-    content: (item: productType) => (
-      <img src={`${config.BACKEND_BASE_URL}/images/products/thumbnails/` + item.thumbnail} alt='thumbnail' width={100} />
+    content: (item: Product) => (
+      <img src={`${config.BACKEND_BASE_URL}/images/products/thumbnails/` + item.thumbnail} alt='thumbnail' width={100} height={100} />
     )
   },
   {
@@ -45,7 +23,7 @@ const tableColumns = [
   {
     label: "دسته‌بندی",
     path: "cat",
-    content: (item: productType) => (
+    content: (item: Product) => (
       <Typography>
         {item.category} / {item.subcategory}
       </Typography>
@@ -67,43 +45,17 @@ const tableColumns = [
   }
 ];
 
-const getProductsOfPage = async (
-  page: number,
-  setProductsCallBack: Dispatch<SetStateAction<never[]>>,
-  setTotalPagesCallBack: Dispatch<SetStateAction<number>>
-) => {
-  try {
-    const { data } = await productService.getProducts(page);
-
-    setTotalPagesCallBack(data.total_pages);
-    const { products } = data.data;
-
-    const subCategories = await Promise.all(
-      products.map((p: productType) => p.subcategory).map((subCatId: string) => subCatService.getSubCatById(subCatId))
-    );
-
-    const richProductItems = products.map((p: productType, i: number) => ({
-      ...p,
-      category: subCategories[i].data.data.subcategory.category.name,
-      subcategory: subCategories[i].data.data.subcategory.name
-    }));
-
-    setProductsCallBack(richProductItems);
-  } catch (error) {}
-};
-
 const AdminProductsPage = () => {
-  const [products, setProducts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
 
-  useEffect(() => {
-    (async () => await getProductsOfPage(currentPage, setProducts, setTotalPages))();
-  }, [currentPage]);
+  const { data, isLoading } = useProducts({ page, perPage });
+  const { products = null, totalCount = 1 } = data ?? {};
 
-  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    event; // just to satisfy typescript :))
-    setCurrentPage(value);
+  const handlePageChange = (page: number) => setPage(page);
+  const handlePerPageChange = (perPage: number) => {
+    setPage(1);
+    setPerPage(perPage);
   };
 
   return (
@@ -118,17 +70,22 @@ const AdminProductsPage = () => {
         </Button>
       </Box>
 
-      <StripedTable columns={tableColumns} rowsData={products} />
+      {isLoading && <LoadingSpinner />}
 
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      {!products && !isLoading && <Typography textAlign='center'>محصولی وجود ندارد! یک محصول جدید اضافه کنید.</Typography>}
+
+      {products && !isLoading && (
         <Pagination
-          count={totalPages}
-          page={currentPage}
-          onChange={handlePageChange}
-          renderItem={item => <PaginationItem slots={{ previous: ArrowForwardIcon, next: ArrowBackIcon }} {...item} />}
-          sx={{ marginTop: 5 }}
-        />
-      </Box>
+          itemsTitle='محصول'
+          itemsCount={totalCount}
+          page={page}
+          perPage={perPage}
+          onPageChange={handlePageChange}
+          onPerPageChange={handlePerPageChange}
+        >
+          <StripedTable columns={tableColumns} rowsData={products} />
+        </Pagination>
+      )}
     </>
   );
 };
