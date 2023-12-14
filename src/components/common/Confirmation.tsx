317f2f98832e69de779a87e90f023db06e031a92
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-14 16:22:00 +0330

    feat: reusable Confirmation component.

---
+++ src/components/common/Confirmation.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from "react";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+interface ConfirmationProps {
+  message: string | ReactElement;
+  confirmTerm: string;
+  onConfirm: (id?: any) => void;
+  onCancel: () => void;
+}
+
+const Confirmation = ({ message, confirmTerm = "تایید", onConfirm, onCancel }: ConfirmationProps) => {
+  return (
+    <div>
+      <Typography sx={{ mb: 4 }}>{message}</Typography>
+
+      <Button variant='contained' color='error' onClick={onConfirm}>
+        {confirmTerm}
+      </Button>
+      <Button variant='outlined' sx={{ mr: 2 }} onClick={onCancel}>
+        انصراف
+      </Button>
+    </div>
+  );
+};
+
+export default Confirmation;
