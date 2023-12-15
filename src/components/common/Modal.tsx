d78eae0bb30bb832668de9ba7d448bbd0a53e7ce
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-15 15:25:06 +0330

    feat: backDropFilter added to the style of Modal component.

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from "react";
+import { styled } from "@mui/material/styles";
 import MuiModal from "@mui/material/Modal";
-import Backdrop from "@mui/material/Backdrop";
+import MuiBackdrop from "@mui/material/Backdrop";
 import Fade from "@mui/material/Fade";
-import Box from "@mui/material/Box";
+import MuiBox from "@mui/material/Box";
 
 interface ModalProps {
   open: boolean;
@@ -10,24 +11,27 @@ interface ModalProps {
   children: ReactElement;
 }
 
-const style = {
+const Box = styled(MuiBox)(({ theme }) => ({
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  borderRadius: 2,
-  boxShadow: 14,
-  p: 4
-};
+  backgroundColor: theme.palette.background.paper,
+  border: "2px solid " + (theme.palette.mode === "light" ? "#000" : "#fff"),
+  borderRadius: 7,
+  boxShadow: "0 0 5px #000",
+  padding: 35
+}));
+
+const Backdrop = styled(MuiBackdrop)({
+  backdropFilter: "blur(10px)"
+});
 
 const Modal = ({ open, onClose, children }: ModalProps) => {
   return (
     <MuiModal open={open} onClose={onClose} closeAfterTransition slots={{ backdrop: Backdrop }} slotProps={{ backdrop: { timeout: 500 } }}>
       <Fade in={open}>
-        <Box sx={style}>{children}</Box>
+        <Box>{children}</Box>
       </Fade>
     </MuiModal>
   );
