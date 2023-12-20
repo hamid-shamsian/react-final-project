5d8ebf8d2a647cafb183df55d53e210c7689016b
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-20 14:55:09 +0330

    feat: bulk edit products full functionality implemented.

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,10 +1,10 @@
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ size }: { size?: number }) => {
   return (
     <Box sx={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center" }}>
-      <CircularProgress />
+      <CircularProgress size={size ?? 40} />
     </Box>
   );
 };
--- src/pages/admin/AdminStockPage.tsx
+++ src/pages/admin/AdminStockPage.tsx
@@ -1,41 +1,116 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import InputBase from "@mui/material/InputBase";
 import StripedTable from "../../components/widget/StripedTable";
 import Pagination from "../../components/common/Pagination";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-import useProducts, { Product } from "../../hooks/useProducts";
+import useProducts from "../../hooks/useProducts";
+import useEditProduct from "../../hooks/useEditProduct";
 
 const tableColumns = [
   {
-    label: "محصول",
+    label: "نام محصول",
     path: "name"
+    // align: "right"
   },
   {
-    label: "موجودی",
-    path: "quantity",
-    content: (item: Product) => <span style={{ textDecoration: "underline", color: "blue" }}>{item.quantity}</span>
+    label: "موجودی در انبار (عدد)",
+    key: "qty",
+    content: (product: any, { onQtyChange, quantities }: any) => (
+      <InputBase
+        type='number'
+        name={product.i}
+        value={quantities[product.i]?.value ?? ""}
+        onChange={onQtyChange}
+        sx={{ px: 2, width: 150, ...(quantities[product.i]?.touched && { backgroundColor: "#000", color: "#fff" }) }}
+      />
+    )
   },
   {
-    label: "قیمت",
-    path: "price",
-    content: (item: Product) => <span style={{ textDecoration: "underline", color: "blue" }}>{item.price}</span>
+    label: "قیمت محصول (تومان)",
+    key: "price",
+    content: (product: any, { onPriceChange, prices }: any) => (
+      <InputBase
+        type='number'
+        name={product.i}
+        value={prices[product.i]?.value ?? ""}
+        onChange={onPriceChange}
+        sx={{ px: 2, width: 150, ...(prices[product.i]?.touched && { backgroundColor: "#000", color: "#fff" }) }}
+      />
+    )
   }
 ];
+
+interface InputsData {
+  touched: boolean;
+  value: number;
+}
 
 const AdminStockPage = () => {
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(5);
 
   const { data, isLoading } = useProducts({ page, perPage });
-  const { products = null, totalCount = 0 } = data ?? {};
+  const { products = [], totalCount = 0 } = data ?? {};
+
+  const bulkEditProducts = useEditProduct(true);
 
   const handlePageChange = (page: number) => setPage(page);
   const handlePerPageChange = (perPage: number) => {
     setPage(1);
     setPerPage(perPage);
   };
+
+  const [quantities, setQuantities] = useState<InputsData[]>([] as InputsData[]);
+  const [prices, setPrices] = useState<InputsData[]>([] as InputsData[]);
+
+  const init = () => {
+    setQuantities(products.map(p => ({ touched: false, value: p.quantity })));
+    setPrices(products.map(p => ({ touched: false, value: p.price })));
+  };
+
+  useEffect(init, [products]);
+
+  const onQtyChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(target.value);
+    if (!isNaN(value) && value > -1) setQuantities(prevQtys => prevQtys.map((d, i) => (i === +target.name ? { touched: true, value } : d)));
+  };
+
+  const onPriceChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(target.value);
+    if (!isNaN(value) && value > -1) setPrices(prevPrices => prevPrices.map((d, i) => (i === +target.name ? { touched: true, value } : d)));
+  };
+
+  const handleSubmit = () => {
+    type TProduct = { quantity?: number; price?: number };
+
+    const touchedProducts: { id: string; product: TProduct }[] = [];
+
+    products.forEach((p, i) => {
+      const product: TProduct = {
+        ...(quantities[i].touched && { quantity: quantities[i].value }),
+        ...(prices[i].touched && { price: prices[i].value })
+      };
+
+      if ((product.price ?? product.quantity) !== undefined) touchedProducts.push({ id: p._id, product });
+    });
+
+    setQuantities(prevQtys => prevQtys.map(q => ({ ...q, touched: false })));
+    setPrices(prevPrices => prevPrices.map(p => ({ ...p, touched: false })));
+
+    bulkEditProducts.mutate(touchedProducts);
+  };
+
+  const handleKeyPress = (event: KeyboardEvent) => event.key === "Escape" && init();
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, []);
+
+  const someInputsChanged = quantities.some(q => q.touched) || prices.some(p => p.touched);
 
   return (
     <>
@@ -44,16 +119,23 @@ const AdminStockPage = () => {
           مدیریت موجودی و قیمت‌ها
         </Typography>
 
-        <Button variant='contained' color='success' disabled>
-          ذخیره تغییرات
-        </Button>
+        <Box>
+          {someInputsChanged && (
+            <Button variant='contained' color='error' onClick={init}>
+              {bulkEditProducts.isPending ? <LoadingSpinner size={24} /> : "انصراف"}
+            </Button>
+          )}
+          <Button variant='contained' color='success' disabled={!someInputsChanged} onClick={handleSubmit} sx={{ width: "150px", mr: 3 }}>
+            {bulkEditProducts.isPending ? <LoadingSpinner size={24} /> : "ثبت تغییرات"}
+          </Button>
+        </Box>
       </Box>
 
       {isLoading && <LoadingSpinner />}
 
-      {!products && !isLoading && <Typography textAlign='center'>محصولی وجود ندارد! یک محصول جدید اضافه کنید.</Typography>}
+      {!products.length && !isLoading && <Typography textAlign='center'>محصولی وجود ندارد! یک محصول جدید اضافه کنید.</Typography>}
 
-      {products && !isLoading && (
+      {products.length && !isLoading && (
         <Pagination
           itemsTitle='محصول'
           itemsCount={totalCount}
@@ -62,7 +144,11 @@ const AdminStockPage = () => {
           onPageChange={handlePageChange}
           onPerPageChange={handlePerPageChange}
         >
-          <StripedTable columns={tableColumns} rowsData={products} />
+          <StripedTable
+            columns={tableColumns}
+            rowsData={products.map((p, i) => ({ ...p, i: String(i) }))}
+            actions={{ qty: { onQtyChange, quantities }, price: { onPriceChange, prices } }}
+          />
         </Pagination>
       )}
     </>
