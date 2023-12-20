57296afeedfffc2f724629f16156cd5a086a3d69
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-20 22:14:50 +0330

    feat: ProductCard component with basic features.

---
+++ src/components/widget/ProductCard.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import Rating from "@mui/material/Rating";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { Product } from "../../services/productService";
+import config from "../../../config.json";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <Link to={`/products/${product._id}`} style={{ textDecoration: "none" }}>
+      <Card sx={{ display: "flex", minWidth: "300px", position: "relative" }}>
+        <img width={150} height={150} src={`${config.BACKEND_BASE_URL}/images/products/thumbnails/${product.thumbnail}`} alt='Product Image' />
+        <Box>
+          <CardContent>
+            <Typography gutterBottom variant='body1' component='h4' noWrap textOverflow='ellipsis' width='130px' overflow='hidden'>
+              {product.name}
+            </Typography>
+          </CardContent>
+          <Typography sx={{ flexGrow: 1 }} variant='body2' paddingX={2}>
+            {product.price} تومان
+          </Typography>
+        </Box>
+        <CardActions sx={{ display: "flex", justifyContent: "end", position: "absolute", left: 0, bottom: 0 }}>
+          <Rating precision={0.5} value={product.rating.rate} size='small' sx={{ direction: "ltr", ml: 1 }} />
+          <IconButton color='info' type='button' onClick={e => e.preventDefault()}>
+            <AddShoppingCartIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,19 @@
+import Box from "@mui/material/Box";
+import ProductCard from "../components/widget/ProductCard";
+import useProducts from "../hooks/useProducts";
+import { Product } from "../services/productService";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const { data } = useProducts({ page: 1, perPage: 10 });
+  const { products = [] } = data ?? {};
+
+  return (
+    <Box sx={{ padding: 5, display: "flex", gap: 3, flexWrap: "wrap" }}>
+      {products.map((p: Product) => (
+        <ProductCard key={p._id} product={p} />
+      ))}
+    </Box>
+  );
 };
 
 export default HomePage;
