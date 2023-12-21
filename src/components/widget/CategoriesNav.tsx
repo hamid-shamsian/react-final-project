4b2b0caf99d3b844e84b4478a1a1f333281694f1
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-22 01:55:44 +0330

    feat: CategoriesNav Routing component with full functionality.

---
+++ src/components/widget/CategoriesNav.tsx
@@ -0,0 +1,85 @@
+import { Fragment, useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import Collapse from "@mui/material/Collapse";
+import ListItemText from "@mui/material/ListItemText";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import IconButton from "@mui/material/IconButton";
+import { ListItemProps } from "@mui/material/ListItem";
+import CustomNavLink from "../common/CustomNavLink";
+import { Category } from "../../services/catService";
+
+interface CategoriesNavProps {
+  categories: Category[];
+}
+
+interface ListItemLinkProps extends ListItemProps {
+  to: string;
+  name: string;
+  open?: boolean;
+  index?: number;
+}
+
+const CategoriesNav = ({ categories }: CategoriesNavProps) => {
+  const [opens, setOpens] = useState<boolean[]>([]);
+
+  useEffect(() => {
+    setOpens(categories.map(_ => Boolean(false)));
+  }, [categories]);
+
+  const handleClick = (i: number) => setOpens(prevOpens => prevOpens.map((o, index) => (index === i ? !o : o)));
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", width: 250 }}>
+      <Box
+        sx={{
+          bgcolor: "background.paper",
+          mt: 1
+        }}
+        component='nav'
+      >
+        <List>
+          {categories.map((cat, i) => (
+            <Fragment key={i}>
+              <ListItemLink to={`/categories/${cat.slugname}`} name={cat.name} open={opens[i]} index={i} />
+              <Collapse component='li' in={opens[i]} timeout='auto' unmountOnExit>
+                <List disablePadding>
+                  {cat.subcategories?.map((subCat, j) => (
+                    <ListItemLink key={`${i}${j}`} to={`/categories/${cat.slugname}/${subCat.slugname}`} name={subCat.name} sx={{ pr: 5 }} />
+                  ))}
+                </List>
+              </Collapse>
+            </Fragment>
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+
+  function ListItemLink({ to, open, name, index, ...other }: ListItemLinkProps) {
+    let icon = null;
+    if (open != null) {
+      icon = open ? <ExpandLess /> : <ExpandMore />;
+    }
+
+    return (
+      <li>
+        <ListItemButton component={CustomNavLink} to={to} {...other}>
+          <ListItemText primary={name} sx={{ textAlign: "right" }} />
+          <IconButton
+            onClick={e => {
+              e.preventDefault();
+              index != null && handleClick(index);
+            }}
+          >
+            {icon}
+          </IconButton>
+        </ListItemButton>
+      </li>
+    );
+  }
+};
+
+export default CategoriesNav;
--- src/pages/CategoriesPage.tsx
+++ src/pages/CategoriesPage.tsx
@@ -1,9 +1,23 @@
 import { useParams } from "react-router-dom";
+import Box from "@mui/material/Box";
+// import CategoryProducts from "../components/widget/CategoryProducts";
+import LoadingSpinner from "../components/common/LoadingSpinner";
+import useCategories from "../hooks/useCategories";
+import CategoriesNav from "../components/widget/CategoriesNav";
 
 const CategoriesPage = () => {
   const { title } = useParams();
 
-  return <div>Categories Page {title}</div>;
+  const { data: categories = [], isLoading } = useCategories();
+
+  return (
+    <Box>
+      {isLoading && <LoadingSpinner />}
+      {/* {categories.length > 0 && !isLoading && categories.map(category => <CategoryProducts key={category._id} category={category} limit={6} />)} */}
+
+      <CategoriesNav categories={categories} />
+    </Box>
+  );
 };
 
 export default CategoriesPage;
