1ae3f3e4b88c963ebbbd807674e7af79d4da1025
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-21 18:56:54 +0330

    feat: Swiper image slider installed and its component implemented.

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,18 +1,27 @@
 import { useParams } from "react-router-dom";
 import Box from "@mui/material/Box";
+// import Typography from "@mui/material/Typography";
 import LoadingSpinner from "../components/common/LoadingSpinner";
 import ErrorPresentation from "../components/common/ErrorPresentation";
+import SwiperSlider from "../components/widget/Swiper/Swiper";
 import useProduct from "../hooks/useProduct";
+import config from "../../config.json";
 
 const ProductPage = () => {
   const { id = "" } = useParams();
 
-  const { isLoading, error } = useProduct(id);
+  const { data: product, isLoading, error } = useProduct(id);
+
+  const images = product?.images.map(image => `${config.BACKEND_BASE_URL}/images/products/thumbnails/${image}`);
 
   return (
     <Box minHeight='100vh'>
       {isLoading && <LoadingSpinner mt={30} />}
       {!isLoading && error && <ErrorPresentation itemTitle='محصول' error={error} />}
+
+      <Box width={500}>
+        <SwiperSlider images={images ?? []} />
+      </Box>
     </Box>
   );
 };
