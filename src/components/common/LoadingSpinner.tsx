677d23f092ee13221e63edafd75b194bf73d1a76
Hamid Shamsian <hamid64bits@gmail.com>
2023-12-21 18:00:48 +0330

    feat: product page error handling implemented.

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,9 +1,9 @@
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 
-const LoadingSpinner = ({ size }: { size?: number }) => {
+const LoadingSpinner = ({ size, mt }: { size?: number; mt?: number }) => {
   return (
-    <Box sx={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center" }}>
+    <Box sx={{ display: "flex", height: "100%", justifyContent: "center", alignItems: "center", mt }}>
       <CircularProgress size={size ?? 40} />
     </Box>
   );
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
+import Box from "@mui/material/Box";
+import LoadingSpinner from "../components/common/LoadingSpinner";
+import ErrorPresentation from "../components/common/ErrorPresentation";
+import useProduct from "../hooks/useProduct";
 
 const ProductPage = () => {
-  const { id } = useParams();
+  const { id = "" } = useParams();
 
-  return <div>Product Details Page. Product ID: {id}</div>;
+  const { isLoading, error } = useProduct(id);
+
+  return (
+    <Box minHeight='100vh'>
+      {isLoading && <LoadingSpinner mt={30} />}
+      {!isLoading && error && <ErrorPresentation itemTitle='محصول' error={error} />}
+    </Box>
+  );
 };
 
 export default ProductPage;
